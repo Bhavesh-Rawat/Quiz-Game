168eb424e1d5a1841c98ea3ff845d63d227a6762
Bhavesh Rawat <bhaveshrawat2002@gmail.com>
2022-10-29 16:59:46 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "JavaScript language is",
+    answer: "Object-oriented scripting language",
+    options: [
+      "Procedural programming language",
+      "Object-oriented scripting language",
+      "Logic programming language",
+      "Query language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Inside which HTML element do we put the JavaScript?",
+    answer: "script",
+    options: [
+      "script",
+      "h1",
+      "js",
+      "p",
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which event occurs when the user clicks on an HTML element?",
+    answer: "onclick",
+    options: [
+      "onmouseover",
+      "onmouseclick",
+      "onclick",
+      "onchange",
+    ]
+  },
+    {
+    numb: 4,
+    question: "JS Stands For ?",
+    answer: "JavaScript",
+    options: [
+      "JavaScript",
+      "JavaSecondio",
+      "JavaStamp",
+      "JavaSpace"
+    ]
+  },
+    {
+    numb: 5,
+    question: "JSX stands for ?",
+    answer: "JavaScript XML",
+    options: [
+      "Java Snchere Xenty",
+      "JavaScript Xtension",
+      "JavaScript Xversion",
+      "JavaScript XML"
+    ]
+  },
+  
+  // You can add question in given below format
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
